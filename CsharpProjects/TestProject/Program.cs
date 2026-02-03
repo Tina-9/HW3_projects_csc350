a59b5f45c7be1c7dd0c1775212e31e10ff398f83
Xin Fu <fay0932@gmail.com>
2026-02-03 10:22:20 -0500

    // Call the methods of a .Net class Library unit 3

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,3 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello C#!");
+
+// Call the methods of a .Net class Library unit 3
+Random dice = new Random(); // create instance of Random class
+int roll = dice.Next(1, 7); // generate random number between 1 and 6
+Console.WriteLine(roll); // display the random number
+
--- CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
+++ CsharpProjects/TestProject/obj/Debug/net10.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8dce856a7b074451884dcc106fc45ee2a583e0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

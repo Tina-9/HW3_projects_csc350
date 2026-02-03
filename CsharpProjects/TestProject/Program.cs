71eb670278512ca2d2d8330c47d9fd75ca931bbe
Xin Fu <fay0932@gmail.com>
2026-02-03 10:35:48 -0500

    Overloaded methods unit 4

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -7,3 +7,10 @@ Random dice = new Random(); // create instance of Random class
 int roll = dice.Next(1, 7); // generate random number between 1 and 6
 Console.WriteLine(roll); // display the random number
 
+//Overloaded methods unit 4
+int number = 7;
+string text = "seven";
+
+Console.WriteLine(number); // calls the method that takes an int
+Console.WriteLine(); // calls the method that takes a string
+Console.WriteLine(text);// calls the method that takes a string
